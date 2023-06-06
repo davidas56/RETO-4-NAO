797bbc35e28211b0d09843aef780c6c78e5e8aee
davidas56 <85710968+davidas56@users.noreply.github.com>
2023-06-06 18:35:18 -0500

    Update js2.js
    v1.3

--- CICLO 1/projectos html/javascript/js2.js
+++ CICLO 1/projectos html/javascript/js2.js
@@ -1,24 +1,37 @@
-
-function loadMoreContent() {
-    setTimeout(function() {
-        var scrollContent = document.getElementById("scroll-content");
-
-        var newContent = document.createElement("div");
-        newContent.className = "scroll-item";
-        newContent.textContent = "Nuevo item";
-        scrollContent.appendChild(newContent);
-
-        checkScrollEnd();
-    }, 1000);
-}
-
-function checkScrollEnd() {
-    var scrollContainer = document.querySelector(".scroll-container");
-    var scrollContent = document.getElementById("scroll-content");
-
-    if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight) {
-        loadMoreContent();
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollContainer = document.getElementById("scroll-container");
+    const loadingElement = document.getElementById("loading");
+  
+    let isLoading = false;
+  
+    function loadMoreContent() {
+      if (isLoading) return;
+  
+      isLoading = true;
+      loadingElement.style.display = "block";
+  
+      setTimeout(function () {
+        // Simulación de carga de contenido
+        const newContent = document.createElement("div");
+        newContent.classList.add("bg-gray-200", "p-4", "mb-4");
+        newContent.innerHTML = "<p>Contenido adicional</p>";
+        scrollContainer.appendChild(newContent);
+  
+        isLoading = false;
+        loadingElement.style.display = "none";
+      }, 2000);
     }
-}
-var scrollContainer = document.querySelector(".scroll-container");
-scrollContainer.addEventListener("scroll", checkScrollEnd);
+  
+    function handleScroll() {
+      const scrollTop = scrollContainer.scrollTop;
+      const scrollHeight = scrollContainer.scrollHeight;
+      const clientHeight = scrollContainer.clientHeight;
+  
+      if (scrollTop + clientHeight >= scrollHeight - 20) {
+        loadMoreContent();
+      }
+    }
+  
+    scrollContainer.addEventListener("scroll", handleScroll);
+  });
+  
